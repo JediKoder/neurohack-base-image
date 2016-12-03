734d46f7a74a11005b5dd297a4cdbe78439d3b6c
Alexander Tiunov <astiunov@yandex-team.ru>
2016-12-03 23:55:47 +0300

    statsmodels, new theano, lasagne

--- install.sh
+++ install.sh
@@ -5,12 +5,18 @@ curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >cond
 
 conda install -y numpy
 
-conda env create --name r_env --file conda_r_env.yml && source activate r_env
-Rscript install_modules.R
-
 conda env create --name py3_env --file conda_py3_env.yml
+source activate py3_env
+pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
+pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
 
 conda env create --name py2_env --file conda_py2_env.yml
+source activate py2_env
+pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
+pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+
+conda env create --name r_env --file conda_r_env.yml && source activate r_env
+Rscript install_modules.R
 
 rm -rf /tmp/* install_modules.R *.yml *.sh && apt-get clean
 
